fed918033c2823972042b51d0a3e632cffd9e442
Shashi Raj <shashiraj.972@gmail.com>
2019-02-03 01:54:42 +0530

    Javascript and zooming Enabled and Displaying the progress dialogue while connecting to internet

--- app/src/main/java/com/shashi/mybrowser/MainActivity.kt
+++ app/src/main/java/com/shashi/mybrowser/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.shashi.mybrowser
 
+import android.app.ProgressDialog
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.webkit.URLUtil
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,21 +18,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         wview.webViewClient = WebViewClient()
+
+        wview.settings.javaScriptEnabled = true //To Enable JavaScript
+        wview.settings.builtInZoomControls = true //To Enable Zoom Controls
+        var pDialog = ProgressDialog(this@MainActivity) //Progress dialogue
+        pDialog.setTitle("Message From Shashi")
+        pDialog.setMessage("Please Wait While loading") //Progress message
+        wview.webViewClient = object:WebViewClient(){
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                pDialog.show()
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                pDialog.dismiss()
+            }
+        }
         var urlAddress = etUrl.text.toString()
         var urlStart = "https://www."
         btYou.setOnClickListener {
             urlAddress = urlStart + "youtube.com"
-            Toast.makeText(this@MainActivity,"...Searching...",Toast.LENGTH_SHORT).show()
             wview.loadUrl(urlAddress)
         }
         btFb.setOnClickListener {
             urlAddress = urlStart + "facebook.com"
+/*
             Toast.makeText(this@MainActivity,"...Searching...",Toast.LENGTH_SHORT).show()
+*/
             wview.loadUrl(urlAddress)
         }
         btGoogle.setOnClickListener {
             urlAddress = urlStart + "google.com"
-            Toast.makeText(this@MainActivity,"...Searching...",Toast.LENGTH_SHORT).show()
             wview.loadUrl(urlAddress)
         }
     }
@@ -40,7 +60,6 @@ class MainActivity : AppCompatActivity() {
             if(!URLUtil.isNetworkUrl(urlAddr)){
                 urlAddr = "https://www.google.com/search?q="+urlAddr
             }
-            Toast.makeText(this@MainActivity,"...Searching...",Toast.LENGTH_SHORT).show()
             wview.loadUrl(urlAddr)
         }
         else{
